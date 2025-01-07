b2e06f37044f538664ff2d6a6f4e4c66b893ca47
Mehdi Essalehi <mehdi.essalehi@gmail.com>
2025-01-07 16:59:40 +0100

    refactor: vault type selection to be more type safe and maintainable

--- resources/ts/components/editor/vault-manager/new-vault-dialog/index.tsx
+++ resources/ts/components/editor/vault-manager/new-vault-dialog/index.tsx
@@ -13,17 +13,32 @@ import {
 } from "@/components/ui/select";
 import { useState } from "react";
 import LocalVaultForm from "./local-vault-form";
+import { Vault } from "@/lib/vaults/types";
 
 type NewVaultDialogProps = {
 	/** The function to call to close the dialog after submit */
 	closeDialog: () => void;
 };
 
+type VaultTypeOption = {
+	name: string;
+	disabled?: boolean;
+}
+
+const supportsFileSystemAPI = typeof window.showDirectoryPicker === "function";
+const vaultTypes: Record<Vault["type"], VaultTypeOption> = {
+	"local": {
+		name: "Local vault",
+		disabled: !supportsFileSystemAPI
+	},
+}
+
 /**
  * Dialog component to add a new vault
  */
 export default function NewVaultDialog({ closeDialog }: NewVaultDialogProps) {
-	const [newVaultType, setNewVaultType] = useState("local");
+
+	const [newVaultType, setNewVaultType] = useState<string | undefined>(supportsFileSystemAPI ? undefined : "local");
 
 	return (
 		<>
@@ -40,14 +55,20 @@ export default function NewVaultDialog({ closeDialog }: NewVaultDialogProps) {
 						<SelectValue id="vault-type" placeholder="Select a vault" />
 					</SelectTrigger>
 					<SelectContent>
-						<SelectItem value="local">Local vault</SelectItem>
+						{Object.keys(vaultTypes).map((vaultType) => {
+							const vaultTypeOption = vaultTypes[vaultType as keyof typeof vaultTypes]
+							return (
+								<SelectItem value={vaultType} disabled={vaultTypeOption.disabled}>
+									{vaultTypeOption.name}
+								</SelectItem>
+							)
+						})}
 					</SelectContent>
 				</Select>
+				<p className="text-sm text-neutral-400">Local vault is only available with browsers that support the File System Access API</p>
 			</div>
-			{newVaultType === "local" ? (
+			{newVaultType === "local" && (
 				<LocalVaultForm closeDialog={closeDialog} />
-			) : (
-				<></>
 			)}
 		</>
 	);
