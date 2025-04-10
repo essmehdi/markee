c8a011ba838a8c47272396753c79199b21fa9701
Mehdi Essalehi <mehdi.essalehi@gmail.com>
2025-04-10 15:24:42 +0100

    fix: clear auto saved draft after saving it

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -1,9 +1,22 @@
+import { ProseMirror } from "@nytimes/react-prosemirror";
+import "katex/dist/katex.min.css";
+import "prosemirror-gapcursor/style/gapcursor.css";
+import { history } from "prosemirror-history";
+import { keymap } from "prosemirror-keymap";
+import { Node } from "prosemirror-model";
+import { EditorState } from "prosemirror-state";
+import { tableEditing } from "prosemirror-tables";
+import "prosemirror-tables/style/tables.css";
+import { NodeViewConstructor } from "prosemirror-view";
+import "prosemirror-view/style/prosemirror.css";
+import { useEffect, useRef, useState } from "react";
 import ImagePicker from "~/components/editor/node-widgets/image-picker";
+import { useToast } from "~/hooks/use-toast";
 import mdSchema from "~/lib/prosemirror/editor-schema";
 import editorKeymap from "~/lib/prosemirror/keymap";
 import {
-  decorationManager,
-  decorator,
+	decorationManager,
+	decorator,
 } from "~/lib/prosemirror/plugins/decorator";
 import footnoter from "~/lib/prosemirror/plugins/footnotes";
 import listItemDecorator from "~/lib/prosemirror/plugins/list-checkbox";
@@ -17,202 +30,193 @@ import HTMLView from "~/lib/prosemirror/views/html-view";
 import MathBlockView from "~/lib/prosemirror/views/math-block-view";
 import useConfirmationAlert from "~/lib/store/confirmation-alert-manager";
 import { useSourceManager } from "~/lib/store/source-manager";
-import { ProseMirror } from "@nytimes/react-prosemirror";
-import "katex/dist/katex.min.css";
-import "prosemirror-gapcursor/style/gapcursor.css";
-import { history } from "prosemirror-history";
-import { keymap } from "prosemirror-keymap";
-import { EditorState } from "prosemirror-state";
-import { tableEditing } from "prosemirror-tables";
-import "prosemirror-tables/style/tables.css";
-import { NodeViewConstructor } from "prosemirror-view";
-import "prosemirror-view/style/prosemirror.css";
-import { useEffect, useRef, useState } from "react";
 import "~/styles/editor.css";
 import Toolbar from "./toolbar";
-import { useToast } from "~/hooks/use-toast";
-import { Node } from "prosemirror-model";
 
 export const nodeViews: { [key: string]: NodeViewConstructor } = {
-  code(node, view, getPos) {
-    return new CodeBlockView(node, view, getPos, mdSchema);
-  },
-  html(node, view, getPos) {
-    return new HTMLView(node, view, getPos, mdSchema);
-  },
-  math_block(node, view, getPos) {
-    return new MathBlockView(node, view, getPos, mdSchema);
-  },
+	code(node, view, getPos) {
+		return new CodeBlockView(node, view, getPos, mdSchema);
+	},
+	html(node, view, getPos) {
+		return new HTMLView(node, view, getPos, mdSchema);
+	},
+	math_block(node, view, getPos) {
+		return new MathBlockView(node, view, getPos, mdSchema);
+	},
 };
 
 export const editorPlugins = [
-  history(),
-  keymap(editorKeymap(mdSchema)),
-  markdownParser,
-  decorator,
-  decorationManager,
-  transformer,
-  textShortcutPlugin,
-  listItemDecorator,
-  footnoter,
-  tableEditing({ allowTableNodeSelection: true }),
-  pasteHandler,
+	history(),
+	keymap(editorKeymap(mdSchema)),
+	markdownParser,
+	decorator,
+	decorationManager,
+	transformer,
+	textShortcutPlugin,
+	listItemDecorator,
+	footnoter,
+	tableEditing({ allowTableNodeSelection: true }),
+	pasteHandler,
 ];
 
-const AUTO_SAVE_DRAFT_INTERVAL = 2000;
+const AUTO_SAVE_DRAFT_INTERVAL = 1000;
 const AUTO_SAVE_DRAFT_LOCAL_STORAGE_KEY = "draft";
 
-// Retrieve auto saved draft doc from local storage
-const localStorageSavedDoc = localStorage.getItem(AUTO_SAVE_DRAFT_LOCAL_STORAGE_KEY);
-let savedDoc: Node | undefined = undefined;
-if (localStorageSavedDoc) {
-  try {
-    savedDoc = Node.fromJSON(mdSchema, JSON.parse(localStorageSavedDoc))
-  } catch (e) {
-    console.error("Could not parse saved doc from local storage.", e);
-  }
-}
+function getEditorDraftInitialState(loadAutoSaveDraft: boolean = true) {
+	// Retrieve auto saved draft doc from local storage
+	let savedDoc: Node | undefined = undefined;
+	if (loadAutoSaveDraft) {
+		const localStorageSavedDoc = localStorage.getItem(
+			AUTO_SAVE_DRAFT_LOCAL_STORAGE_KEY,
+		);
+		if (localStorageSavedDoc) {
+			try {
+				savedDoc = Node.fromJSON(mdSchema, JSON.parse(localStorageSavedDoc));
+			} catch (e) {
+				console.error("Could not parse saved doc from local storage.", e);
+			}
+		}
+	}
 
-// Initialize editor state with empty draft or auto saved draft. 
-export const editorInitialState = EditorState.create({
-  schema: mdSchema,
-  plugins: editorPlugins,
-  doc: savedDoc
-});
+	// Initialize editor state with empty draft or auto saved draft.
+	return EditorState.create({
+		schema: mdSchema,
+		plugins: editorPlugins,
+		doc: savedDoc,
+	});
+}
 
 /**
  * Main editor component
  */
 export default function Editor() {
-  const {
-    isCurrentSourceDeleted,
-    isLoadingSource,
-    currentSelection,
-    currentSource,
-    setIsLoadingSource,
-    changeCurrentSource,
-    changeCurrentSourceDeletedFlag,
-    setLastSaveState,
-    checkDocSavedState,
-  } = useSourceManager();
-  const { showConfirmationAlert } = useConfirmationAlert();
-  const { toast } = useToast();
+	const {
+		isCurrentSourceDeleted,
+		isLoadingSource,
+		currentSelection,
+		currentSource,
+		setIsLoadingSource,
+		changeCurrentSource,
+		changeCurrentSourceDeletedFlag,
+		setLastSaveState,
+		checkDocSavedState,
+	} = useSourceManager();
+	const { showConfirmationAlert } = useConfirmationAlert();
+	const { toast } = useToast();
 
-  const [mount, setMount] = useState<HTMLElement | null>(null);
-  const [editorState, setEditorState] =
-    useState<EditorState>(editorInitialState);
-  const draftAutoSaveTimeout = useRef<ReturnType<typeof setTimeout>>(null);
+	const [mount, setMount] = useState<HTMLElement | null>(null);
+	const [editorState, setEditorState] = useState<EditorState>(
+		getEditorDraftInitialState(),
+	);
+	const draftAutoSaveTimeout = useRef<ReturnType<typeof setTimeout>>(null);
 
-  /**
-   * Reads the selected file and sets it as the source of the
-   * editor. Set the last save hash to the content of the file.
-   */
-  const loadSource = () => {
-    const doLoadSource = () => {
-      setIsLoadingSource(true);
+	/**
+	 * Reads the selected file and sets it as the source of the
+	 * editor. Set the last save hash to the content of the file.
+	 */
+	const loadSource = () => {
+		const doLoadSource = () => {
+			setIsLoadingSource(true);
 
-      const { vault, file } = currentSelection;
-      vault!
-        .getFileContent(file!)
-        .then((decodedFileContent) => {
-          const doc = getNewDocFromMarkdown(decodedFileContent);
-          const newEditorState = EditorState.create({
-            schema: mdSchema,
-            plugins: editorPlugins,
-            doc,
-          });
+			const { vault, file } = currentSelection;
+			vault!
+				.getFileContent(file!)
+				.then((decodedFileContent) => {
+					const doc = getNewDocFromMarkdown(decodedFileContent);
+					const newEditorState = EditorState.create({
+						schema: mdSchema,
+						plugins: editorPlugins,
+						doc,
+					});
 
-          changeCurrentSource({
-            vault: vault!,
-            file: file!,
-          });
+					changeCurrentSource({
+						vault: vault!,
+						file: file!,
+					});
 
-          setEditorState(newEditorState);
-          setLastSaveState(newEditorState);
-        })
-        .catch((error) => {
-          console.error("Could not open file", error);
-          toast({
-            title: "Could not open file",
-            description: "An error occured while opening the file",
-          });
-          setLastSaveState(null);
-        })
-        .finally(() => {
-          setIsLoadingSource(false);
-        });
-    };
+					setEditorState(newEditorState);
+					setLastSaveState(newEditorState);
+				})
+				.catch((error) => {
+					console.error("Could not open file", error);
+					toast({
+						title: "Could not open file",
+						description: "An error occured while opening the file",
+					});
+					setLastSaveState(null);
+				})
+				.finally(() => {
+					setIsLoadingSource(false);
+				});
+		};
 
-    if (
-      currentSelection.vault &&
-      currentSelection.file &&
-      (currentSelection.vault !== currentSource?.vault ||
-        currentSelection.file !== currentSource?.file)
-    ) {
-      if (currentSource && !checkDocSavedState(editorState)) {
-        showConfirmationAlert(
-          doLoadSource,
-          "Are you sure?",
-          "This will replace the current document and all unsaved content will be lost and cannot be recovered. The history will be reset consequently."
-        );
-      } else {
-        doLoadSource();
-      }
-    }
-  };
+		if (
+			currentSelection.vault &&
+			currentSelection.file &&
+			(currentSelection.vault !== currentSource?.vault ||
+				currentSelection.file !== currentSource?.file)
+		) {
+			if (currentSource && !checkDocSavedState(editorState)) {
+				showConfirmationAlert(
+					doLoadSource,
+					"Are you sure?",
+					"This will replace the current document and all unsaved content will be lost and cannot be recovered. The history will be reset consequently.",
+				);
+			} else {
+				doLoadSource();
+			}
+		}
+	};
 
-  /**
-   * Clears the editor when the current source is deleted.
-   * Resets the flags after clearing.
-   */
-  const clearSource = () => {
-    const newEditorState = EditorState.create({
-      schema: mdSchema,
-      plugins: editorPlugins,
-    });
-    setEditorState(newEditorState);
-    setLastSaveState(null);
-    changeCurrentSource(null);
-    changeCurrentSourceDeletedFlag(false);
-  };
+	/**
+	 * Clears the editor when the current source is deleted.
+	 * Resets the flags after clearing.
+	 */
+	const clearSource = () => {
+		const newEditorState = getEditorDraftInitialState();
+		setEditorState(newEditorState);
+		setLastSaveState(null);
+		changeCurrentSource(null);
+		changeCurrentSourceDeletedFlag(false);
+	};
 
-  useEffect(() => {
-    loadSource();
-  }, [currentSelection]);
+	useEffect(() => {
+		loadSource();
+	}, [currentSelection]);
 
-  useEffect(() => {
-    if (isCurrentSourceDeleted) {
-      clearSource();
-    }
-  }, [isCurrentSourceDeleted]);
+	useEffect(() => {
+		if (isCurrentSourceDeleted) {
+			clearSource();
+		}
+	}, [isCurrentSourceDeleted]);
 
-  useEffect(() => {
-    if (!currentSource) {
-      if (draftAutoSaveTimeout.current) {
-        clearTimeout(draftAutoSaveTimeout.current);
-      }
-      draftAutoSaveTimeout.current = setTimeout(() => {
-        localStorage.setItem(
-          AUTO_SAVE_DRAFT_LOCAL_STORAGE_KEY,
-          JSON.stringify(editorState.doc.toJSON())
-        );
-      }, AUTO_SAVE_DRAFT_INTERVAL);
-    }
-  }, [editorState, currentSource]);
+	useEffect(() => {
+		if (!currentSource) {
+			if (draftAutoSaveTimeout.current) {
+				clearTimeout(draftAutoSaveTimeout.current);
+			}
+			draftAutoSaveTimeout.current = setTimeout(() => {
+				localStorage.setItem(
+					AUTO_SAVE_DRAFT_LOCAL_STORAGE_KEY,
+					JSON.stringify(editorState.doc.toJSON()),
+				);
+			}, AUTO_SAVE_DRAFT_INTERVAL);
+		}
+	}, [editorState, currentSource]);
 
-  return (
-    <ProseMirror
-      mount={mount}
-      state={editorState}
-      dispatchTransaction={(tr) => {
-        setEditorState((oldState) => oldState.apply(tr));
-      }}
-      nodeViews={nodeViews}
-      editable={() => !isLoadingSource}
-    >
-      <Toolbar />
-      <ImagePicker />
-      <div className="md-editor" ref={setMount} />
-    </ProseMirror>
-  );
+	return (
+		<ProseMirror
+			mount={mount}
+			state={editorState}
+			dispatchTransaction={(tr) => {
+				setEditorState((oldState) => oldState.apply(tr));
+			}}
+			nodeViews={nodeViews}
+			editable={() => !isLoadingSource}
+		>
+			<Toolbar />
+			<ImagePicker />
+			<div className="md-editor" ref={setMount} />
+		</ProseMirror>
+	);
 }
--- src/components/editor/main-toolbar/menu/save-as/save-as-dialog.tsx
+++ src/components/editor/main-toolbar/menu/save-as/save-as-dialog.tsx
@@ -1,4 +1,3 @@
-import { useEditorState } from "@nytimes/react-prosemirror";
 import { Check, CircleNotch } from "@phosphor-icons/react";
 import { DialogTitle } from "@radix-ui/react-dialog";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
@@ -11,11 +10,11 @@ import { DialogHeader } from "~/components/ui/dialog";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
 } from "~/components/ui/select";
 import { getMarkdownFromDocAsync } from "~/lib/prosemirror/serialization/serializer";
 import useDialog from "~/lib/store/dialog-manager";
@@ -27,143 +26,148 @@ import LocalVault from "~/lib/vaults/local-vault";
 import { Vault, VaultDirectory } from "~/lib/vaults/types";
 
 type SaveAsDialogProps = {
-  editorState: EditorState;
+	editorState: EditorState;
 };
 
 export default function SaveAsDialog({ editorState }: SaveAsDialogProps) {
-  const queryClient = useQueryClient();
-  const changeCurrentSource = useSourceManager(
-    (state) => state.changeCurrentSource
-  );
-  const setLastSaveState = useSourceManager((state) => state.setLastSaveState);
-  const [vault, setVault] = useState<Vault | null>(null);
-  const [selectedDirectory, setSelectedDirectory] = useState<VaultDirectory>(
-    BaseLocalVault.ROOT_DIRECTORY
-  );
-  const [fileName, setFileName] = useState<string>("");
-  const [error, setError] = useState<string>("");
-  const [isSaving, setIsSaving] = useState(false);
-  const { closeDialog } = useDialog();
+	const queryClient = useQueryClient();
+	const currentSource = useSourceManager((state) => state.currentSource);
+	const changeCurrentSource = useSourceManager(
+		(state) => state.changeCurrentSource,
+	);
+	const setLastSaveState = useSourceManager((state) => state.setLastSaveState);
+	const [vault, setVault] = useState<Vault | null>(null);
+	const [selectedDirectory, setSelectedDirectory] = useState<VaultDirectory>(
+		BaseLocalVault.ROOT_DIRECTORY,
+	);
+	const [fileName, setFileName] = useState<string>("");
+	const [error, setError] = useState<string>("");
+	const [isSaving, setIsSaving] = useState(false);
+	const { closeDialog } = useDialog();
 
-  const {
-    data: vaults,
-    isLoading,
-    error: vaultFetchError,
-  } = useQuery({
-    queryKey: ["vaults"],
-    queryFn: async () => [
-      ...(await LocalVault.getAllLocalVaultsFromIndexedDB()),
-      ...(await BrowserVault.getAllBrowserVaults()),
-    ],
-  });
+	const {
+		data: vaults,
+		isLoading,
+		error: vaultFetchError,
+	} = useQuery({
+		queryKey: ["vaults"],
+		queryFn: async () => [
+			...(await LocalVault.getAllLocalVaultsFromIndexedDB()),
+			...(await BrowserVault.getAllBrowserVaults()),
+		],
+	});
 
-  const setSelectedVault = (vaultId: string) => {
-    const vault = vaults!.find((vault) => vault.id === vaultId)!;
-    setVault(vault);
-  };
+	const setSelectedVault = (vaultId: string) => {
+		const vault = vaults!.find((vault) => vault.id === vaultId)!;
+		setVault(vault);
+	};
 
-  const saveFile = () => {
-    setError("");
-    if (fileName.length === 0) {
-      setError("Please specify a valid file name");
-      return;
-    }
-    if (selectedDirectory === null) {
-      setError("Please choose a directory");
-      return;
-    }
-    let correctedFileName = fileName;
-    if (
-      !fileName.toLowerCase().endsWith(".txt") &&
-      !fileName.toLowerCase().endsWith(".md")
-    ) {
-      correctedFileName = fileName + ".md";
-    }
+	const saveFile = () => {
+		setError("");
+		if (fileName.length === 0) {
+			setError("Please specify a valid file name");
+			return;
+		}
+		if (selectedDirectory === null) {
+			setError("Please choose a directory");
+			return;
+		}
+		let correctedFileName = fileName;
+		if (
+			!fileName.toLowerCase().endsWith(".txt") &&
+			!fileName.toLowerCase().endsWith(".md")
+		) {
+			correctedFileName = fileName + ".md";
+		}
 
-    setIsSaving(true);
-    vault
-      ?.createFile(selectedDirectory, correctedFileName)
-      .then((file) => {
-        getMarkdownFromDocAsync(editorState)
-          .then((serialized) => {
-            vault
-              .writeToFile(file, serialized)
-              .then(() => {
-                changeCurrentSource({ vault: vault, file });
-                setLastSaveState(editorState);
-                queryClient.invalidateQueries({
-                  queryKey: ["vault", vault.id],
-                });
-                closeDialog();
-              })
-              .catch((e) => {
-                console.error(e);
-                setError("An error occured while writing to file");
-              })
-              .finally(() => setIsSaving(false));
-          })
-          .catch((e) => {
-            console.error(e);
-            setError("An error occured while serializing file");
-          });
-      })
-      .catch((e: ConflictError) => {
-        console.error(e);
-        setIsSaving(false);
-        setError(e.message);
-      });
-  };
+		setIsSaving(true);
+		vault
+			?.createFile(selectedDirectory, correctedFileName)
+			.then((file) => {
+				getMarkdownFromDocAsync(editorState)
+					.then((serialized) => {
+						vault
+							.writeToFile(file, serialized)
+							.then(() => {
+								const wasDraft = currentSource === null;
+								changeCurrentSource({ vault: vault, file });
+								setLastSaveState(editorState);
+								queryClient.invalidateQueries({
+									queryKey: ["vault", vault.id],
+								});
+								if (wasDraft) {
+									localStorage.removeItem("draft");
+								}
+								closeDialog();
+							})
+							.catch((e) => {
+								console.error(e);
+								setError("An error occured while writing to file");
+							})
+							.finally(() => setIsSaving(false));
+					})
+					.catch((e) => {
+						console.error(e);
+						setError("An error occured while serializing file");
+					});
+			})
+			.catch((e: ConflictError) => {
+				console.error(e);
+				setIsSaving(false);
+				setError(e.message);
+			});
+	};
 
-  return (
-    <>
-      <DialogHeader>
-        <DialogTitle>Choose where to save</DialogTitle>
-      </DialogHeader>
-      <div className="space-y-5">
-        <Select value={vault?.id ?? undefined} onValueChange={setSelectedVault}>
-          <SelectTrigger className="w-full">
-            <SelectValue
-              placeholder={isLoading ? "Loading vaults..." : "Select a vault"}
-            />
-          </SelectTrigger>
-          <SelectContent position="popper">
-            {vaults?.map((vault) => (
-              <SelectItem key={vault.id} value={vault.id}>
-                <VaultSelectItem vault={vault} />
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-        {vault && (
-          <>
-            <DirectoryPicker
-              className="w-full h-48"
-              vault={vault}
-              onSelect={setSelectedDirectory}
-            />
-            <div>
-              <Label>File name</Label>
-              <Input
-                className="w-full"
-                name="file-name"
-                value={fileName}
-                onChange={(e) => setFileName(e.target.value)}
-              />
-            </div>
-          </>
-        )}
-        <div className="space-y-2">
-          {error !== "" && <p className="text-destructive text-sm">{error}</p>}
-          <Button
-            className="h-full"
-            disabled={isSaving || vault === null || fileName === ""}
-            onClick={saveFile}
-          >
-            {isSaving ? <CircleNotch className="animate-spin" /> : <Check />}
-            Save
-          </Button>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<DialogHeader>
+				<DialogTitle>Choose where to save</DialogTitle>
+			</DialogHeader>
+			<div className="space-y-5">
+				<Select value={vault?.id ?? undefined} onValueChange={setSelectedVault}>
+					<SelectTrigger className="w-full">
+						<SelectValue
+							placeholder={isLoading ? "Loading vaults..." : "Select a vault"}
+						/>
+					</SelectTrigger>
+					<SelectContent position="popper">
+						{vaults?.map((vault) => (
+							<SelectItem key={vault.id} value={vault.id}>
+								<VaultSelectItem vault={vault} />
+							</SelectItem>
+						))}
+					</SelectContent>
+				</Select>
+				{vault && (
+					<>
+						<DirectoryPicker
+							className="w-full h-48"
+							vault={vault}
+							onSelect={setSelectedDirectory}
+						/>
+						<div>
+							<Label>File name</Label>
+							<Input
+								className="w-full"
+								name="file-name"
+								value={fileName}
+								onChange={(e) => setFileName(e.target.value)}
+							/>
+						</div>
+					</>
+				)}
+				<div className="space-y-2">
+					{error !== "" && <p className="text-destructive text-sm">{error}</p>}
+					<Button
+						className="h-full"
+						disabled={isSaving || vault === null || fileName === ""}
+						onClick={saveFile}
+					>
+						{isSaving ? <CircleNotch className="animate-spin" /> : <Check />}
+						Save
+					</Button>
+				</div>
+			</div>
+		</>
+	);
 }
