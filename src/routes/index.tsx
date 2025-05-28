e56724339209e376261203c23f7f2e71dca32ef4
Mehdi Essalehi <mehdi.essalehi@gmail.com>
2025-05-28 23:25:24 +0100

    feat: changed editor route to index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router'
+import Editor from "~/components/editor";
+import BaseLayout from "~/components/layouts/base-layout";
+import BaseSidebar from "~/components/layouts/base-sidebar";
+import { SidebarInset } from "~/components/ui/sidebar";
 
 export const Route = createFileRoute('/')({
-  component: Home,
+  component: EditorPage,
+	ssr: false
 })
 
-function Home() {
-  return (
-    <div className="p-2">
-      <h3>Welcome Home!!!</h3>
-    </div>
-  )
+/**
+ * Editor page
+ */
+function EditorPage() {
+	return (
+		<BaseLayout>
+			<BaseSidebar />
+			<SidebarInset className="@container/main w-full">
+				<Editor />
+			</SidebarInset>
+		</BaseLayout>
+	);
 }
