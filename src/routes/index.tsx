3f67ba7bae60ca9e8fb4714d6c7a261f03f608d7
Mehdi Essalehi <mehdi.essalehi@gmail.com>
2025-05-29 00:37:01 +0100

    fix: seo tags

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,25 @@ import BaseLayout from "~/components/layouts/base-layout";
 import BaseSidebar from "~/components/layouts/base-sidebar";
 import { SidebarInset } from "~/components/ui/sidebar";
 
+const title = import.meta.env.VITE_APP_NAME;
+const description = "A web-based Markdown editor with live preview";
+const keywords = "markdown, editor, live preview, web-based, open source";
+
 export const Route = createFileRoute("/")({
   component: EditorPage,
   ssr: false,
   head: () => ({
     meta: [
-      {
-        title: import.meta.env.VITE_APP_NAME,
-        description: "A live preview web-based Markdown editor",
-      },
+      { title },
+      { name: "description", content: description },
+      { name: "keywords", content: keywords },
+      { name: "twitter:title", content: title },
+      { name: "twitter:description", content: description },
+      { name: "twitter:creator", content: "@mehdiessalehi" },
+      { name: "twitter:site", content: "@mehdiessalehi" },
+      { name: "og:type", content: "website" },
+      { name: "og:title", content: title },
+      { name: "og:description", content: description },
     ],
   }),
 });
